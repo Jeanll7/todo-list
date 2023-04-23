979695f95c33e402e55ec0a647a28b41734348bf
devjean <jeancarloleall@gmail.com>
2023-04-23 07:38:01 -0300

    project-todo-list

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import TodoTask from "./components/TodoTask/TodoTask";
+import { useState } from "react";
+import "./styles/styles.css";
+
+interface ITask {
+  id: number;
+  nameTask: string;
+}
 
 function App() {
+  const [task, setTask] = useState("");
+
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  function addTask() {
+    const idRandom = (num: number) => Math.floor(Math.random() * num);
+
+    const newTask = { id: idRandom(9999999), nameTask: task };
+
+    setTodoList([...todoList, newTask]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h2>Lists</h2>
+
+        <input
+          type="text"
+          autoComplete="off"
+          placeholder="Escrever task..."
+          name="task"
+          className="input"
+          value={task}
+          onChange={(event) => setTask(event.target.value)}
+        />
+
+        <button type="submit" onClick={addTask} className="btn-header">
+          Adicionar Task
+        </button>
       </header>
+
+      <div className="line"></div>
+
+      {/* <TodoTask /> */}
+
+      {todoList.map((task) => (
+        <div>
+          {task.nameTask}
+          {task.id}
+        </div>
+      ))}
     </div>
   );
 }
